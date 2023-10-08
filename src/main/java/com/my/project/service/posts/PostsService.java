461ed3e858ac39b78714e6312131f041bcfa4c52
sudarling <sumin8072@gmail.com>
2023-10-09 02:05:16 +0900

    JPA를 이용한 API 추가 - 게시판 등록, 수정, 조회 API 추가 - JPA Auditing 적용

---
+++ src/main/java/com/my/project/service/posts/PostsService.java
@@ -0,0 +1,37 @@
+package com.my.project.service.posts;
+
+import com.my.project.domain.posts.Posts;
+import com.my.project.domain.posts.PostsRepository;
+import com.my.project.web.dto.PostsResponseDto;
+import com.my.project.web.dto.PostsSaveRequestDto;
+import com.my.project.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto postsSaveRequestDto) {
+        return postsRepository.save(postsSaveRequestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public PostsResponseDto findById(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(posts);
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+}
--- src/test/java/com/my/project/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/my/project/domain/posts/PostsRepositoryTest.java
@@ -2,6 +2,7 @@ package com.my.project.domain.posts;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.After;
 import org.junit.Test;
@@ -37,6 +38,24 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2023, 10, 9, 0, 0, 0);
+        postsRepository.save(Posts.builder().title("title").content("content").author("author").build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>>>>>> createDate=" + posts.getCreatedDate() + ", modifiedDate=" + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 
 }
